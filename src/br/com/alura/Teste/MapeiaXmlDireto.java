124c586b15c630512847a13d1d8c2e265f5fef33
leandro-gft <lpsacchi@gmail.com>
2020-09-15 16:57:29 -0300

    Mapeando de XML para objeto

---
+++ src/br/com/alura/Teste/MapeiaXmlDireto.java
@@ -0,0 +1,25 @@
+package br.com.alura.Teste;
+
+import java.io.File;
+
+import javax.xml.bind.JAXB;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import br.com.alura.Model.Venda;
+
+public class MapeiaXmlDireto {
+	
+	public static void main(String[] args) throws JAXBException {
+		JAXBContext jaxbContext = JAXBContext.newInstance(Venda.class);
+		
+		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+		
+		Venda venda = (Venda) unmarshaller.unmarshal(new File("src/vendas.xml"));
+		
+		System.out.println(venda);		
+		
+	}
+
+}
